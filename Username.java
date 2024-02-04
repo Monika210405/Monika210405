00ac985750f149c8c5db8bf61a0cdedb0bd5b1ef
Monika210405 <158811271+Monika210405@users.noreply.github.com>
2024-02-04 20:31:27 +0530

    Username.java

---
+++ Username.java
@@ -0,0 +1,12 @@
+Import java.util.Scanner;
+class Username
+{
+  Public static void main(String[] argos)
+  {
+    String user;
+    System.out.print(s:”Enter user name: “);
+    Scanner input = new Scanner(System.in);
+    User = input.nextLine();
+    System.out.print(“Hello “ + user);
+  }
+ }
