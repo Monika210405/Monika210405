46f004f7464bb47fb2b84e77c270d0e063f9cd7b
Monika210405 <158811271+Monika210405@users.noreply.github.com>
2024-02-11 10:14:29 +0530

    Update Hellouser.cpp

--- week2/Hellouser.cpp
+++ week2/Hellouser.cpp
@@ -3,13 +3,8 @@
 
 int main() {
     std::string username;
-
-    // Prompting the user to enter their name
     std::cout << "Please enter your name: ";
     std::getline(std::cin, username);
-
-    // Displaying the greeting message
     std::cout << "Hello " << username << "!" << std::endl;
-
     return 0;
 }
