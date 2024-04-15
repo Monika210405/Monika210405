f375a174c436873abfd2c22553cbda33309ef3ef
Monika210405 <158811271+Monika210405@users.noreply.github.com>
2024-04-15 22:33:35 +0530

    Create boxUnbox.java

---
+++ week8/boxUnbox.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args)
+  {
+      Integer a=new Integer(100);
+      Integer b=new Integer(200);
+
+      int c=b;
+      System.out.println("The boxed Value is : "+a);
+      System.out.println("The unBoxed Value is : "+c);
+  }
+}
