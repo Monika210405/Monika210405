2e7c77cf6ff6344df06d0c89b546d3672b40ac2d
Monika210405 <158811271+Monika210405@users.noreply.github.com>
2024-04-24 21:50:29 +0530

    Update filestream.java

--- week9/filestream.java
+++ week9/filestream.java
@@ -3,28 +3,26 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class CopyFile
-{
-  public static void main(String[] args)
-  {
-    try
-    {
-      FileInputStream fin=new FileInputStream("source") ;
-      FileOutputStream fout=new FileOutputStream("Destination");
-      while(fin.available()!=0)
-      {
-        fout.write(fin.read());
-      }
-      fin.close();
-      fout.close();
+public class Main{
+    public static void main(String[] args) {
+        FileInputStream filein =null;
+        FileOutputStream fileout=null;
+        try{
+            filein =new FileInputStream("fileIn.txt");
+            fileout=new FileOutputStream("fileOut.txt");
+            try {
+                System.out.println("Total Number of Bytes : "+filein.available());
+                int i;
+                while ((i=filein.read())!=-1) {
+                    fileout.write(i);
+                }
+                filein.close();
+                fileout.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred while performing input/output operations.");
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("The File is not found here");
+        }
     }
-    catch(FileNotFoundException obj)
-    {
-      System.out.println(“Invalid file name”);
-    }
-    catch(IOException obj)
-    {
-      System.out.println(“Invalid input or output”);
-    }
-  }
 }
