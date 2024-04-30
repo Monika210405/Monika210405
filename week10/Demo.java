193991cf17c8e28143a10bed6aa531c58bfeeb43
Monika210405 <158811271+Monika210405@users.noreply.github.com>
2024-04-30 05:58:24 +0530

    Add files via upload

---
+++ week10/Demo.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="Demo.class" width="1000" height="1000"> </applet>
+*/
+
+@SuppressWarnings("removal")
+public class Demo extends Applet {
+	
+	public void paint(Graphics g) {
+		g.drawLine(10,10,100,200);
+	}
+}
